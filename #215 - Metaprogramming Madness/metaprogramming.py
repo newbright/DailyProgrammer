09218b45792b3b1fdc859d6021e2c9f5e4a321f7
Brandon Newbright <newbright.2@osu.edu>
2015-06-04 15:31:12 -0400

    Add clarification to #215

--- #215 - Metaprogramming Madness/metaprogramming.py
+++ #215 - Metaprogramming Madness/metaprogramming.py
@@ -7,5 +7,6 @@
 
 types_list = [1, 0, 1.0, 0.0, 'c', '', "Hello World!", "", [1, 2, 3], [], True, False]
 
+print "EXPRESSION : BOOLEAN"
 for item in types_list:
 	print str(item) + " : " + str(bool(item))
